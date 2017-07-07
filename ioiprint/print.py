a8a2bf14d18decee9a322761fa4f73f0df43a457
Eqbal Sarjami <eqbalsarjami1994@gmail.com>
2017-07-07 20:37:30 +0430

    Add actual printing command

--- ioiprint/print.py
+++ ioiprint/print.py
@@ -1,4 +1,6 @@
+import subprocess
 
 
 def print_file(file_path, printer):
-    print("Printing %s on %s!" % (file_path, printer))
+    subprocess.run(['lpr', '-P', printer, '-o', 'fit-to-page', file_path],
+                   check=True)
