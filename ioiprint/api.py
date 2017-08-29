f6fa5dededdf162dff52e3773adeadfb091916e4
Eqbal Sarjami <eqbalsarjami1994@gmail.com>
2017-08-30 00:35:56 +0430

    Accept path beside url for desk image

--- ioiprint/api.py
+++ ioiprint/api.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 from ioiprint import DEFAULT_PRINTER, PDF_UPLOAD_PATH, PRINTER_FOR_ZONE
 from ioiprint.modifier import make_cms_request_pdf, make_contestant_pdf, \
     make_translation_pdf
-from ioiprint.netadmin import get_contestant_data
+from ioiprint.contestant_data import get_contestant_data
 from ioiprint.print import print_file
 from ioiprint.utils import create_temp_directory, download
 
@@ -59,8 +59,11 @@ def cms_request():
     ip = request.form['ip']
     contestant_data = get_contestant_data(ip)
     temp_directory = create_temp_directory()
-    desk_map_img = download(contestant_data['desk_image_url'], 'desk_map.svg',
-                            temp_directory)
+    if 'desk_image_url' in contestant_data:
+        desk_map_img = download(contestant_data['desk_image_url'],
+                                'desk_map.svg', temp_directory)
+    else:
+        desk_map_img = contestant_data['desk_image_path']
     request_pdf_path = make_cms_request_pdf(
         request_message,
         contestant_data['contestant_id'],
@@ -80,8 +83,11 @@ def contestant():
     cups_job_id = request.form['cups_job_id']
     contestant_data = get_contestant_data(ip)
     temp_directory = create_temp_directory()
-    desk_map_img = download(contestant_data['desk_image_url'], 'desk_map.svg',
-                            temp_directory)
+    if 'desk_image_url' in contestant_data:
+        desk_map_img = download(contestant_data['desk_image_url'],
+                                'desk_map.svg', temp_directory)
+    else:
+        desk_map_img = contestant_data['desk_image_path']
     final_pdf_path = make_contestant_pdf(
         os.path.join(PDF_UPLOAD_PATH, filename),
         contestant_data['contestant_id'],
