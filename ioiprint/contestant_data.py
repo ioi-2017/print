38ee1294c9943dd0fb2ab80d4dd6297685f82176
Printing <jhasanov@gmail.com>
2019-07-23 19:17:54 +0400

    basic changes

--- ioiprint/contestant_data.py
+++ ioiprint/contestant_data.py
@@ -5,13 +5,13 @@ from ioiprint import CONTESTANT_DATA_ADDRESS_URL, NET_ADMIN_URL
 
 
 def get_contestant_data(ip):
-    url_address = CONTESTANT_DATA_ADDRESS_URL.format(ip=ip)
-    data = json.loads(urlopen(url_address).read().decode('utf-8'))
+    #url_address = CONTESTANT_DATA_ADDRESS_URL.format(ip=ip)
+    #data = json.loads(urlopen(url_address).read().decode('utf-8'))
     return {
-        'contestant_id': data['contestant']['id'],
-        'contestant_name': data['contestant']['name'],
-        'contestant_country': data['contestant']['country'],
-        'zone': data['desk']['zone'],
-        'desk_id': data['desk']['id'],
+        'contestant_id': '3',#data['contestant']['id'],
+        'contestant_name': 'Jamal Hasanov', #data['contestant']['name'],
+        'contestant_country': 'AZE', #data['contestant']['country'],
+        'zone': 'A', #data['desk']['zone'],
+        'desk_id': '13', #data['desk']['id'],
         'desk_image_url': 'http://' + NET_ADMIN_URL + data['desk']['map']
     }
